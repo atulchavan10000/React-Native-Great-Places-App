a6c6bd2364c60152322e5c03f88531b084099901
Atul <atulchavan528@gmail.com>
2020-05-27 15:28:54 +0530

    places stored on db with insertPlace method, auto generated id from the db is passed to action and fetched at reducer

--- store/actions/places-actions.js
+++ store/actions/places-actions.js
@@ -1,6 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 
 export const ADD_PLACE = 'ADD_PLACE';
+import { insertPlace } from '../../helpers/db';
 
 export const addPlace = (title, image) => {
     return async dispatch => {
@@ -14,17 +15,19 @@ export const addPlace = (title, image) => {
                 from: image,
                 to: newPath
             });
+            const dbResult = await insertPlace(
+                title, 
+                newPath, 
+                'Dummy Address', 
+                15.6, 
+                12.3
+            );
+            console.log(dbResult);
+            dispatch({type: ADD_PLACE, placeData: {id: dbResult.insertId, title: title, image: newPath } });
         } catch (err) {
             console.log(err);
             throw err;
         }
-
-        dispatch({
-            type: ADD_PLACE, placeData: {
-                title: title,
-                image: newPath
-            }
-        });
     }
 
 }
