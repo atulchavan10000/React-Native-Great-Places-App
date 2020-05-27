6b2fdce8311795cd5b57b103f86b6a8d81e983c1
Atul <atulchavan528@gmail.com>
2020-05-27 15:58:12 +0530

    fetch places records from db and display them on PlacesListScreen

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import PlaceItem from '../components/PlaceItem';
+import * as placesActions from '../store/actions/places-actions';
 
 const PlacesListScreen = props => {
-  const places = useSelector(state => state.places.places)
+  const places = useSelector(state => state.places.places);
+  const dispatch = useDispatch();
+  
+
+  /*
+  *dispatch action on component load and dispatch only 
+  *once by providing 'dispatch' as a dependency
+  */
+  useEffect(() =>{
+    dispatch(placesActions.loadPlaces());
+  }, [dispatch]);
+
   return (
     <FlatList 
       data ={places}
