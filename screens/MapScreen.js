d56fd152640517c86f41d93c87c9af077dda0a3a
Atul <atulchavan528@gmail.com>
2020-11-08 22:27:31 +0530

    making picked loc savable and storing picked loc

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import { Platform } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import {
     View,
     Text,
     StyleSheet
 } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Colors from '../constants/Colors';
 
 const MapScreen = props =>{
     const [selectedLocation, setSelectedLocation] = useState(); 
@@ -26,6 +29,17 @@ const MapScreen = props =>{
         })
     };
 
+    const savePickedLocationHandler = useCallback(() => {
+        if(!selectedLocation){
+            return;
+        }
+        props.navigation.navigate('NewPlace', {pickedLocation: selectedLocation});
+    }, [selectedLocation]);
+
+    useEffect(() => {
+        props.navigation.setParams({saveLocation: savePickedLocationHandler})
+    }, [savePickedLocationHandler]);
+    
     let markerCoordinates;
     if(selectedLocation){
         markerCoordinates = {
@@ -36,14 +50,31 @@ const MapScreen = props =>{
     return (
        <MapView style={styles.map} region={mapRegion} onPress={selectLocationHandler}>
            {markerCoordinates && <Marker title= 'Picked Location' coordinate={markerCoordinates}></Marker>}
-           
        </MapView>
     )
-}
+};
+
+MapScreen.navigationOptions = navData =>{
+    const saveFn = navData.navigation.getParam('saveLocation');
+    return {
+        headerRight: (
+                <TouchableOpacity style={styles.headerButton} onPress={saveFn}>
+                    <Text style={styles.headerButtonText}>Save</Text>
+                </TouchableOpacity>
+            )
+    }
+};
 
 const styles = StyleSheet.create({
     map: {
         flex:1
+    },
+    headerButton: {
+        marginHorizontal: 20
+    },
+    headerButtonText:{
+        fontSize: 16,
+        color: Platform.OS === 'android' ? 'white' : Colors.primary
     }
 });
 export default MapScreen;
